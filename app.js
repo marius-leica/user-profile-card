6756de06564f90ad7abaf8977f6e67d503918d1d
Marius L <mariusleica@gmail.com>
2022-10-29 18:48:48 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let userBox = document.getElementById("userBox");
+function openInfo() {
+  userBox.classList.remove("hide-details");
+}
+function closeInfo() {
+  userBox.classList.add("hide-details");
+}
